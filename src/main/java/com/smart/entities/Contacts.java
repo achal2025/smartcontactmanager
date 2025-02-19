c3a954b7b8bede0c5e2acf8549576af5041abf90
Achal gavande <Achal gavande@DESKTOP-HAP8BAV>
2025-02-19 15:14:57 +0530

    My First Application

---
+++ src/main/java/com/smart/entities/Contacts.java
@@ -0,0 +1,84 @@
+package com.smart.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Contact")
+public class Contacts {
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private int cId;
+	private String name;
+	private String work;
+	private String email;
+	private String phone;
+	private String image;
+	@Column(length=1000)
+	private String about;
+	
+	@ManyToOne
+	private User user;
+	
+	public Contacts() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getcId() {
+		return cId;
+	}
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWork() {
+		return work;
+	}
+	public void setWork(String work) {
+		this.work = work;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+	
+
+}
